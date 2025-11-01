19f44844f8cf9c218358d5b3105a17b2bb3499b8
Ahmad-Abbas-1 <abbs.os.ahmad@gmail.com>
2025-11-01 23:10:58 +0100

    allergenen overzicht added

--- resources/views/products/allergenen-info.blade.php
+++ resources/views/products/allergenen-info.blade.php
@@ -7,6 +7,42 @@
     <title>Overzicht Allergenen</title>
 </head>
 <body>
+    <div class="container d-flex justify-content-center">
+        <div class="col-md-8">
+            <h1>{{ $title }}</h1>
 
+                @if($product)
+                    <div class="card mt-4 mb-4">
+                        <div class="card-body">
+                            <p><strong>Naam:</strong> {{ $product->ProductNaam }}</p>
+                            <p><strong>Barcode:</strong> {{ $product->Barcode }}</p>
+                        </div>
+                    </div>
+                @endif
+
+                <table class="table table-striped table-bordered align-middle shadow-sm">
+                    <thead>
+                        <tr>
+                            <th>Naam</th>
+                            <th>Omschrijving</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($allergenen as $allergeen)
+                            <tr>
+                                <td>{{ $allergeen->Naam }}</td>
+                                <td>{{ $allergeen->Omschrijving }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="2" class="text-center">Geen allergenen beschikbaar</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+
+                <a href="{{ route('product.index') }}" class="btn btn-secondary mt-3">Terug naar overzicht</a>
+        </div>
+    </div>
 </body>
 </html>
