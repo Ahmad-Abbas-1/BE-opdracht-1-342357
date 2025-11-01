1560fc232bd009edcfc9ab69d408b87b0ed51a78
Ahmad-Abbas-1 <abbs.os.ahmad@gmail.com>
2025-11-01 22:21:26 +0100

    GetProducts SP added

---
+++ database/createscripts/SP_GetProducts.sql
@@ -0,0 +1,25 @@
+use laravel;
+
+DROP PROCEDURE IF EXISTS sp_GetProducts;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_GetProducts()
+BEGIN
+	
+    SELECT PROD.Id
+		  ,PROD.Naam
+          ,PROD.Barcode
+          ,MAGA.VerpakkingsEenheid
+          ,MAGA.AantalAanwezig
+          
+	FROM Product AS PROD
+    
+    INNER JOIN Magazijn AS MAGA
+    
+    ON PROD.Id = MAGA.ProductId;
+
+
+END$$
+
+DELIMITER ;
