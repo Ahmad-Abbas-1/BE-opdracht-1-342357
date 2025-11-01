ae55b965ac2fe748299e5731159c2f786b07b006
Ahmad-Abbas-1 <abbs.os.ahmad@gmail.com>
2025-11-01 22:49:18 +0100

    link naar product/index added

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,6 +22,10 @@ Route::delete('/allergeen/{id}', [AllergeenController::class, 'destroy'])->name(
 Route::get('/allergeen/{id}/edit', [AllergeenController::class, 'edit'])->name('allergeen.edit');
 
 Route::put('/allergeen/{id}', [AllergeenController::class, 'update'])->name('allergeen.update');
+
+
+Route::get('/', [ProductController::class, 'index'])->name('product.index');
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
